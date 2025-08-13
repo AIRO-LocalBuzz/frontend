eea41b74b189f56e84e21d8f37d6c8c1c828d30c
JongWon Lee <106072775+LJW22222@users.noreply.github.com>
2025-08-13 14:58:12 +0900

    fix: Import Error Fix

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SplashPage.css';
 import { useNavigate } from 'react-router-dom';
-import Statusbar from '../../components/statusBar';
+import Statusbar from '../../components/StatusBar';
 
 const SplashPage = () => {
     const navigate = useNavigate();
