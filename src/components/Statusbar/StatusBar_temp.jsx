260009d5b6f6a76390a046ab55e9db9f6d5dc8c7
seino <dhtp0112@gmail.com>
2025-08-14 22:49:31 +0900

    Add StatusBar_temp to version control

---
+++ src/components/Statusbar/StatusBar_temp.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import './StatusBar.css';
+import networkIcon from '../../assets/icons/statusbar/icon-network.svg';
+import wifiIcon from '../../assets/icons/statusbar/icon-wifi.svg';
+import batteryIcon from '../../assets/icons/statusbar/icon-battery.svg';
+
+export default function StatusBar() {
+    const [time, setTime] = useState(getCurrentTime());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(getCurrentTime());
+        }, 60 * 1000);
+
+        setTime(getCurrentTime());
+
+        return () => clearInterval(interval);
+    }, []);
+
+    function getCurrentTime() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+    }
+
+    return (
+        <>
+            <div className='status_bar'>
+                <div className='time'>
+                    <p>{time}</p>
+                </div>
+                <div className='levels'>
+                    <img
+                        className='network'
+                        src={networkIcon}
+                        alt='network'
+                    />
+                    <img
+                        className='wifi'
+                        src={wifiIcon}
+                        alt='wifi'
+                    />
+                    <img
+                        className='battery'
+                        src={batteryIcon}
+                        alt='battery'
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
